13b8810fd45e1e11fd8c9be5592383ba9d7ad49a
Matt Welke <mattwelke@gmail.com>
2022-08-21 14:31:58 -0400

    Add linting with semistandard (#138)
    * Add eslint via "npm init @eslint/config".
    
    * Use semistandard instead of eslint.
    
    * Add GHA workflow to lint code.
    
    * Fix code formatting for semistandard.
    
    * Fix "Missing space before function parentheses."
    
    * Switch from "var" to "let" and "const" in test files only (keeping support for old IE).
    
    * Fix code formatting to follow semistandard, with some exceptions for old IE support and testing.
    
    * Remove space-before-function-paren exception.
    
    * Add back space-before-function-paren disable.
    
    * Apply default VS Code JS formatting.

--- misc/node-test.js
+++ misc/node-test.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 var inspector = require('../');
 
 var schema = {
--- test/test.js
+++ test/test.js
@@ -1,9 +1,14 @@
-var validation = require('./validation_test').validation;
-var sanitization = require('./sanitization_test').sanitization;
-var generator = require('./generator_test').generator;
+/* global suite */
+/* eslint no-extend-native: ["error", { "exceptions": ["Array"] }] */
+// Disable space-before-function-paren for compatibility with VS Code default JS formatter.
+/* eslint-disable space-before-function-paren */
+
+const validation = require('./validation_test').validation;
+const sanitization = require('./sanitization_test').sanitization;
+const generator = require('./generator_test').generator;
 
 // testing issues with shims
-Array.prototype.TMP = function(){};
+Array.prototype.TMP = function () { };
 
 suite('Validation', validation);
 suite('Sanitization', sanitization);
