181f127c1b9d5f109fb6d6902273431719903c9d
Matt Welke <mattwelke@gmail.com>
2022-08-21 13:14:38 -0400

    Fix code formatting for semistandard.

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ var sanitization = require('./sanitization_test').sanitization;
 var generator = require('./generator_test').generator;
 
 // testing issues with shims
-Array.prototype.TMP = function(){};
+Array.prototype.TMP = function () { };
 
 suite('Validation', validation);
 suite('Sanitization', sanitization);
